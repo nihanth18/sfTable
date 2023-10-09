52f05cbd86bea8931cf4d650789c103fdf867180
Heisenberg <89655618+nihanth18@users.noreply.github.com>
2023-10-09 14:10:49 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,37 +2,32 @@ import streamlit as st
 import snowflake.connector
 import pandas as pd
 from faker import Faker
+import toml  # Import the toml library to parse the TOML file
 
 fake = Faker()
 
 def generate_data():
     data = {}
     column_names = []
-    
+
     for _ in range(10):
-        column_name = fake.word().replace(' ', '_')[:20]  # Generate a simplified column name (up to 20 characters)
+        column_name = fake.word().replace(' ', '_')[:20]
         column_names.append(column_name)
 
-    # Generate data for each column
     for column_name in column_names:
         column_values = []
         for _ in range(10):
-            column_value = fake.word()  # Generate a random value
+            column_value = fake.word()
             column_values.append(column_value)
         data[column_name] = column_values
-    
+
     return data
 
 data = [generate_data() for _ in range(10)]
 
-snowflake_config = {
-    'account': 'wgpapyx-perficient_partner',
-    'user': 'KNIHANTH',
-    'password': 'ThomasShelby@^123',
-    'warehouse': 'TRANSFORM_WH',
-    'database': 'HANDSHAKE_DB',
-    'schema': 'PRACTICE_N'
-}
+# Read Snowflake configuration from config.toml
+config_data = toml.load("config.toml")
+snowflake_config = config_data["SNOWFLAKE"]
 
 def get_snowflake_connection():
     return snowflake.connector.connect(**snowflake_config)
@@ -56,7 +51,7 @@ if st.button("Save Data to Snowflake"):
         # Create Snowflake table dynamically based on the column names
         create_table_sql = "CREATE TABLE generated_data ("
         for column_name in generated_data_df.columns:
-            create_table_sql += f'{column_name} STRING, '  # Remove double quotes here
+            create_table_sql += f'{column_name} STRING, '
         create_table_sql = create_table_sql.rstrip(', ') + ")"
         cursor.execute(create_table_sql)
 
@@ -65,7 +60,7 @@ if st.button("Save Data to Snowflake"):
             insert_sql = "INSERT INTO generated_data ("
             values_sql = "VALUES ("
             for column_name in generated_data_df.columns:
-                insert_sql += f'{column_name}, '  # Remove double quotes here
+                insert_sql += f'{column_name}, '
                 values_sql += f'\'{row[column_name]}\', '
             insert_sql = insert_sql.rstrip(', ') + ") "
             values_sql = values_sql.rstrip(', ') + ")"
